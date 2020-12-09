93aadf5458d4203f44162adde11aa8ff708a7765
CleitonLuizOliveira <cleitoneldorn@gmail.com>
2020-12-09 15:25:37 -0300

    agora funciona, só tá feio

--- src/App.js
+++ src/App.js
@@ -6,25 +6,20 @@ function App() {
     const [answer, setAnswer] = useState(0);
     const [birthDay, setBirthDay] = useState(0);
 
-    const today = new Date("2020-12-09");
-
-    // function CalculateAnswer() {
-    //     const newAnswer = Math.ceil(Math.abs(new Date() - birthDay) / (1000 * 60 * 60 * 24));
-    //     setAnswer(newAnswer);
-    // }
+    const today = new Date();
+    const realToday = `${today.getFullYear()}-${today.getMonth() + 1}-${today.getDay()}`
 
     useEffect(() => {
-        const newAnswer = Math.ceil(Math.abs(today - birthDay) / (1000 * 60 * 60 * 24));
+        const newAnswer = Math.ceil(Math.abs(new Date(realToday) - new Date(birthDay)) / (1000 * 60 * 60 * 24));
         setAnswer(newAnswer);
-    }, [birthDay])
+    }, [birthDay, realToday]);
 
     return (
         <div className="App">
             <h2>How many days have you lived until today?</h2>
             <label htmlFor="">Select yout birthday below and you will know!</label>
             <input type="date" value={birthDay} onChange={event => setBirthDay(event.currentTarget.value) } />
-            <legend>{birthDay}</legend>
-            <legend>Up until today you have lived {answer} days!</legend>
+            <h3>Up until today you have lived {answer} days!</h3>
         </div>
     );
 }
