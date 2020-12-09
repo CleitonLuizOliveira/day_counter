e550fa80fbbb360a38be63e91a27a573e5085464
CleitonLuizOliveira <cleitoneldorn@gmail.com>
2020-12-09 15:16:08 -0300

    criei o projeto, ainda não funciona direito

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [answer, setAnswer] = useState(0);
+    const [birthDay, setBirthDay] = useState(0);
+
+    const today = new Date("2020-12-09");
+
+    // function CalculateAnswer() {
+    //     const newAnswer = Math.ceil(Math.abs(new Date() - birthDay) / (1000 * 60 * 60 * 24));
+    //     setAnswer(newAnswer);
+    // }
+
+    useEffect(() => {
+        const newAnswer = Math.ceil(Math.abs(today - birthDay) / (1000 * 60 * 60 * 24));
+        setAnswer(newAnswer);
+    }, [birthDay])
+
+    return (
+        <div className="App">
+            <h2>How many days have you lived until today?</h2>
+            <label htmlFor="">Select yout birthday below and you will know!</label>
+            <input type="date" value={birthDay} onChange={event => setBirthDay(event.currentTarget.value) } />
+            <legend>{birthDay}</legend>
+            <legend>Up until today you have lived {answer} days!</legend>
+        </div>
+    );
 }
 
 export default App;
